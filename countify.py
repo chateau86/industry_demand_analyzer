71e4b085dd496dd1dc06a44f5ddb00edc9b4ba6e
Woradorn K <chateau86@gmail.com>
2018-06-05 14:24:08 +0700

    Added word counter.
    Signed-off-by: Woradorn K <chateau86@gmail.com>

--- countify.py
+++ countify.py
@@ -2,6 +2,19 @@
 
 import glob
 import pprint
+import re
+from collections import Counter
 
-pprint.pprint(list(glob.iglob(r'fetch_page\gh_pages\*')))
-pprint.pprint(list(glob.iglob(r'fetch_page\lever_pages\*')))
+file_list = []
+file_list += glob.iglob(r'fetch_page\gh_pages\*')
+file_list += glob.iglob(r'fetch_page\lever_pages\*')
+#pprint.pprint(file_list)
+word_count = Counter()
+for file_name in file_list:
+    with open(file_name,'r') as f_in:
+        w_arr = map(str.lower, re.sub('[\t\?\!\xa0\n\*]',' ',f_in.read().strip()).split(' '))
+        w_set = set(w_arr)
+        for w in w_set:
+            word_count[w] += 1
+        #print(w_arr)
+pprint.pprint(word_count)
